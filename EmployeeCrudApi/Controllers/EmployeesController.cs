f6f3e23a3fb81029d714dc322c1fe75795eaf90a
Bikrant226 <bikrantthakur6@gmail.com>
2024-11-12 22:13:04 +0530

    Add project files.

---
+++ EmployeeCrudApi/Controllers/EmployeesController.cs
@@ -0,0 +1,91 @@
+﻿using EmployeeCrudApi.Data;
+using EmployeeCrudApi.Models;
+using EmployeeCrudApi.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeCrudApi.Controllers
+{
+    //localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context; //to access DbContext
+
+        //Constructor Ingestion
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this._context = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = _context.Employees.ToList();
+            return Ok(allEmployees);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeesById(Guid id) // parameter has to be same as mentioned in Route 
+        {
+            var employee = _context.Employees.Find(id);
+
+            if (employee == null) 
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto dto)
+        {
+            var employeeEntity = new Employee() 
+            { 
+                Name = dto.Name,
+                Email = dto.Email,
+                Phone = dto.Phone,
+                Salary = dto.Salary
+            };
+
+            _context.Employees.Add(employeeEntity);
+            _context.SaveChanges(); //this is mandatory for saving the changes 
+            return Ok(employeeEntity);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id,UpdateEmployeeDto dto)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            employee.Name = dto.Name;
+            employee.Email = dto.Email;
+            employee.Phone = dto.Phone;
+            employee.Salary = dto.Salary;
+
+            _context.SaveChanges(); //this is mandatory for saving the changes
+
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee= _context.Employees.Find(id);
+            if (employee == null) 
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return Ok(employee);
+        }
+    }
+}
---
+++ EmployeeCrudApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeCrudApi.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCrudApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
